3863f81f2d0d3dbc8930eaf5499fbf88709cd939
Shin <vanvietkhtn@gmail.com>
2025-10-14 15:05:59 +0700

    7 - Makemigrations and migrate with Django

--- src/courses/models.py
+++ src/courses/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class AccessRequirement(models.TextChoices):
     ANYONE = "any", "Anyone"
-    EMAIL_REQUIRED = "email_required", "Email required"
+    EMAIL_REQUIRED = "email", "Email required"
 
 
 class PublishStatus(models.TextChoices):
@@ -12,13 +12,18 @@ class PublishStatus(models.TextChoices):
     DRAFT = "draft", "Draft"
 
 
+def handle_upload(instance, filename):
+    return f"{filename}"
+
+
 class Course(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
     access = models.CharField(
-        max_length=10,
+        max_length=5,
         choices=AccessRequirement.choices,
-        default=AccessRequirement.ANYONE,
+        default=AccessRequirement.EMAIL_REQUIRED,
     )
     status = models.CharField(
         max_length=10, choices=PublishStatus.choices, default=PublishStatus.DRAFT
