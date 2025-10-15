867a8efaad19207cabf748fba43e4d70625c1eb4
Shin <vanvietkhtn@gmail.com>
2025-10-15 20:07:59 +0700

    use django template render video

--- src/helpers/_cloudinary/services.py
+++ src/helpers/_cloudinary/services.py
@@ -1,3 +1,7 @@
+from django.conf import settings
+from django.template.loader import get_template
+
+
 def get_cloudinary_image_object(instance, field_name="image", as_html=False, width=200):
     if not hasattr(instance, field_name):
         return None
@@ -50,5 +54,9 @@ def get_cloudinary_video_object(
         video_option["crop"] = "limit"
     url = video_object.build_url(**video_option)
     if as_html:
-        return video_html.format(video_url=url)
+        template_name = "videos/snippets/embed.html"
+        tmpl = get_template(template_name)
+        cloud_name = settings.CLOUDINARY_CLOUD_NAME
+        _html = tmpl.render({"video_url": url, "cloud_name": cloud_name})
+        return _html
     return url
--- src/sfehome/settings.py
+++ src/sfehome/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 LOCAL_CDN = BASE_DIR.parent / "local-cdn"
+TEMPLATE_DIR = BASE_DIR / "templates"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -56,7 +58,7 @@ ROOT_URLCONF = "sfehome.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [TEMPLATE_DIR],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -126,3 +128,8 @@ MEDIA_ROOT = LOCAL_CDN / "media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# config cloudinary
+CLOUDINARY_CLOUD_NAME = config("CLOUDINARY_CLOUD_NAME")
+CLOUDINARY_PUBLIC_API_KEY = config("CLOUDINARY_PUBLIC_API_KEY")
+CLOUDINARY_SECRET_API_KEY = config("CLOUDINARY_SECRET_API_KEY")
