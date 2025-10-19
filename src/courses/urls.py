69fc4f2239658f5b7926905540c2a5ec4314395e
Shin <vanvietkhtn@gmail.com>
2025-10-19 16:32:23 +0700

    improve home page

--- src/courses/urls.py
+++ src/courses/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = "courses"
 
 urlpatterns = [
-    path("<slug:course_id>/lessons/<slug:lesson_id>", views.lesson_detail_view),
-    path("<slug:course_id>/", views.course_detail_view),
-    path("", views.course_list_view),
+    path(
+        "<slug:course_id>/lessons/<slug:lesson_id>",
+        views.lesson_detail_view,
+        name="lesson_detail",
+    ),
+    path("<slug:course_id>/", views.course_detail_view, name="detail"),
+    path("", views.course_list_view, name="list"),
 ]
